1b898cf4319b02e74ef995726d313c8f5b171e6a
amiehusseinSSoori <amirhusseinsoori75@gmail.com>
2021-10-09 07:08:57 -0700

    initial repository

---
+++ app/src/main/java/com/amirhusseinsoori/edge_of_android_technology/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.amirhusseinsoori.edge_of_android_technology
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.amirhusseinsoori.edge_of_android_technology.ui.theme.EdgeofAndroidTechnologyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EdgeofAndroidTechnologyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EdgeofAndroidTechnologyTheme {
+        Greeting("Android")
+    }
+}
