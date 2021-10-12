a1baa76fbbe922e935423130a70a50de10afe19d
amiehusseinSSoori <amirhusseinsoori75@gmail.com>
2021-10-12 01:24:23 -0700

    add store4

--- app/src/main/java/com/amirhusseinsoori/edge_of_android_technology/data/romote/ApiServices.kt
+++ app/src/main/java/com/amirhusseinsoori/edge_of_android_technology/data/romote/ApiServices.kt
@@ -2,6 +2,7 @@ package com.amirhusseinsoori.edge_of_android_technology.data.romote
 
 import com.amirhusseinsoori.edge_of_android_technology.BuildConfig
 import com.amirhusseinsoori.edge_of_android_technology.model.remote.Movies
+import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,4 +13,11 @@ interface ApiServices {
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("page") page: Int
     ): Movies
+
+
+    @GET("tv/popular")
+     fun getPopularMoviesWithFlow(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("page") page: Int
+    ): Flow<Movies>
 }
--- app/src/main/java/com/amirhusseinsoori/edge_of_android_technology/repository/MovieRepositoryImp.kt
+++ app/src/main/java/com/amirhusseinsoori/edge_of_android_technology/repository/MovieRepositoryImp.kt
@@ -3,6 +3,8 @@ package com.amirhusseinsoori.edge_of_android_technology.repository
 import com.amirhusseinsoori.edge_of_android_technology.data.local.AppDataBase
 import com.amirhusseinsoori.edge_of_android_technology.model.remote.Movies
 import com.amirhusseinsoori.edge_of_android_technology.data.romote.ApiServices
+import com.dropbox.android.external.store4.Fetcher
+import com.dropbox.android.external.store4.StoreBuilder
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -17,4 +19,9 @@ class MovieRepositoryImp
         //appDataBase.userDao().getMovieByPage()
         emit(apiServices.getPopularMovies(page = 1))
     }
+
+
+    val store = StoreBuilder
+        .from(Fetcher.ofFlow { articleId -> apiServices.getPopularMoviesWithFlow(page = 1) }) // api returns Flow<Article>
+        .build()
 }
